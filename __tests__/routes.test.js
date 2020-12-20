8d1b84b08205e3823b489f6a378aadf589aaa026
Kleo <>
2020-12-20 00:08:12 -0500

    added more routes (AUTH) tests

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -8,11 +8,8 @@ let token = null;
 let refresh = null;
 
 beforeAll((done) => {
-  server = m_app.listen(3000, () => {
-    console.log("Testing server started");
-    global.agent = request.agent(server);
-    done();
-  });
+  server = m_app.listen(3000);
+  done();
 });
 
 afterAll(async (done) => {
@@ -24,56 +21,233 @@ afterAll(async (done) => {
   refresh = null;
 });
 
-describe("AUTH Route", () => {
+describe("AUTH ROUTE", () => {
   // register
-  it("Should register new user", async (done) => {
-    const body = await request(m_app).post("/api/auth/register").send({
-      firstname: "Admin",
-      lastname: "Lastname",
-      email: "admin@admin.com",
-      password: "admin123",
+  describe("Register endpoint", () => {
+    it("Should register new user", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toBe(201);
+      expect(body.body.desc).toBe("PASS");
+      done();
     });
-    expect(body.status).toBe(201);
-    expect(body.body.desc).toBe("PASS");
-    done();
+
+    it("Should fail to register new user without (firstname)", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "",
+        lastname: "Lastname",
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("First name can not be empty");
+      done();
+    });
+
+    it("Should fail to register new user without (firstname) as string", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: 123,
+        lastname: "Lastname",
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("First name is not valid");
+      done();
+    });
+
+    it("Should fail to register new user without (lastname)", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "",
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Last name can not be empty");
+      done();
+    });
+
+    it("Should fail to register new user without (lastname) as string", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: 123,
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Last name is not valid");
+      done();
+    });
+
+    it("Should fail to register new user without (email)", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Email can not be empty");
+      done();
+    });
+
+    it("Should fail to register new user without (email) invalid", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "admin",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Not a valid email");
+      done();
+    });
+
+    it("Should fail to register new user without (password)", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "admin@admin.com",
+        password: "",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Password can not be empty");
+      done();
+    });
+
+    it("Should fail to register new user without (password) between 8 - 1024 chars length", async (done) => {
+      const body = await request(m_app).post("/api/auth/register").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "admin@admin.com",
+        password: "123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe(
+        "Password must contain at least 8 or more characters"
+      );
+      done();
+    });
   });
 
   // login
-  it("Should login user", async (done) => {
-    const body = await request(m_app).post("/api/auth/login").send({
-      email: "admin@admin.com",
-      password: "admin123",
+  describe("Login endpoint", () => {
+    it("Should login user", async (done) => {
+      const body = await request(m_app).post("/api/auth/login").send({
+        email: "admin@admin.com",
+        password: "admin123",
+      });
+      expect(body.status).toEqual(200);
+      expect(body.body.desc).toBe("PASS");
+      token = body.headers["authorization"];
+      refresh = body.headers["x-refresh"];
+      expect(token).not.toBeNull();
+      expect(token).toContain("Bearer");
+      expect(refresh).not.toBeNull();
+      done();
     });
-    expect(body.status).toEqual(200);
-    expect(body.body.desc).toBe("PASS");
-    token = body.headers["authorization"];
-    refresh = body.headers["x-refresh"];
-    expect(token).not.toBeNull();
-    expect(token).toContain("Bearer");
-    expect(refresh).not.toBeNull();
-    done();
+
+    it("Should fail to login user without (email)", async (done) => {
+      const body = await request(m_app).post("/api/auth/login").send({
+        email: "",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Email can not be empty");
+      done();
+    });
+
+    it("Should fail to login user if (email) is invalid", async (done) => {
+      const body = await request(m_app).post("/api/auth/login").send({
+        firstname: "Admin",
+        lastname: "Lastname",
+        email: "admin",
+        password: "admin123",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Not a valid email");
+      done();
+    });
+
+    it("Should fail to login user without (password)", async (done) => {
+      const body = await request(m_app).post("/api/auth/login").send({
+        email: "admin@admin.com",
+        password: "",
+      });
+      expect(body.status).toBe(200);
+      expect(body.body.desc).toBe("FAIL");
+      expect(body.body.msg).toBe("Password can not be empty");
+      done();
+    });
   });
 
   // refresh
-  it("Should send a new refresh token", async (done) => {
-    const body = await request(m_app).get("/api/auth/refresh-token").set({
-      "x-refresh": refresh,
+  describe("Refresh endpoint", () => {
+    it("Should send a new refresh token", async (done) => {
+      const body = await request(m_app).get("/api/auth/refresh-token").set({
+        "x-refresh": refresh,
+      });
+      expect(body.status).toEqual(200);
+      expect(body.body.desc).toBe("PASS");
+      token = body.headers["authorization"];
+      expect(token).not.toBeNull();
+      expect(token).toContain("Bearer");
+      done();
     });
-    expect(body.status).toEqual(200);
-    expect(body.body.desc).toBe("PASS");
-    token = body.headers["authorization"];
-    expect(token).not.toBeNull();
-    expect(token).toContain("Bearer");
-    done();
+
+    it("Should fail to send a new refresh token without x-refresh", async (done) => {
+      const body = await request(m_app).get("/api/auth/refresh-token");
+      expect(body.status).toEqual(400);
+      done();
+    });
+
+    it("Should fail to send a new refresh token without matching x-refresh", async (done) => {
+      const body = await request(m_app).get("/api/auth/refresh-token").set({
+        "x-refresh": "not real token",
+      });
+      expect(body.status).toEqual(401);
+      done();
+    });
   });
 
   // logout
-  it("Should logout user", async (done) => {
-    const body = await request(m_app)
-      .delete("/api/auth/logout")
-      .set({ authorization: token });
-    expect(body.status).toEqual(200);
-    expect(body.body.desc).toBe("PASS");
-    done();
+  describe("Logout endpoint", () => {
+    it("Should logout user", async (done) => {
+      const body = await request(m_app)
+        .delete("/api/auth/logout")
+        .set({ authorization: token });
+      expect(body.status).toEqual(200);
+      expect(body.body.desc).toBe("PASS");
+      done();
+    });
+
+    it("Should fail to logout user without authorization token", async (done) => {
+      const body = await request(m_app).delete("/api/auth/logout");
+      expect(body.status).toEqual(401);
+      done();
+    });
+
+    it("Should fail to logout user without valid authorization token", async (done) => {
+      const body = await request(m_app)
+        .delete("/api/auth/logout")
+        .set({ authorization: "not valid token" });
+      expect(body.status).toEqual(401);
+      done();
+    });
   });
 });
